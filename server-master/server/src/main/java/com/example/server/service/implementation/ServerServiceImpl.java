c79ee6e204f4109b76b2e55e41b615fc0d3039de
Vitaliy Dorosh <maxlevelx@gmail.com>
2023-03-26 14:39:20 +0300

    Initial commit

---
+++ server-master/server/src/main/java/com/example/server/service/implementation/ServerServiceImpl.java
@@ -0,0 +1,70 @@
+package com.example.server.service.implementation;
+
+import com.example.server.enumeration.Status;
+import com.example.server.model.Server;
+import com.example.server.repo.ServerRepo;
+import com.example.server.service.ServerService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Collection;
+import java.util.Random;
+
+import static java.lang.Boolean.TRUE;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
+public class ServerServiceImpl implements ServerService {
+    private final ServerRepo serverRepo;
+    @Override
+    public Server create(Server server) {
+        log.info("Saving new server:{}", server.getName());
+        server.setImageUrl(setServerImageURl());
+        return serverRepo.save(server);
+    }
+
+    @Override
+    public Server ping(String ipAdress) throws IOException {
+        log.info("Pining server IP:{}", ipAdress);
+        Server server = serverRepo.findByIpAdress(ipAdress);
+        InetAddress address = InetAddress.getByName(ipAdress);
+        server.setStatus(address.isReachable(10000)? Status.SERVER_UP:Status.SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        log.info("Fetching all servers");
+        return serverRepo.findAll(PageRequest.of(0,limit)).toList();
+    }
+
+    @Override
+    public Server get(Long id) {
+        log.info("fetching server by id: {}", id);
+        return serverRepo.findById(id).get();
+    }
+
+    @Override
+    public Server update(Server server) {
+        log.info("Updating server: {}", server.getName());
+        return serverRepo.save(server);
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        log.info("Deleting server by id: {}", id);
+        serverRepo.deleteById(id);
+        return TRUE;
+    }
+    public String setServerImageURl() {
+        String[] imageNames = {"server1.png","server2.png","server3.png","server4.png"};
+        return ServletUriComponentBiulder.fromCurrentContextPatg().path("/server/image" + imageNames(new Random().nextInt(4))).toString;
+    }
